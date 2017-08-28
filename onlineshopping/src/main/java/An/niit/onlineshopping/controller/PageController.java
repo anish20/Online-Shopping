7bba85acbf925259d496602214bcb6ae35f1b738
anish20 <anish15.786@gmail.com>
2017-08-28 16:30:24 +0530

    updated

---
+++ onlineshopping/src/main/java/An/niit/onlineshopping/controller/PageController.java
@@ -0,0 +1,21 @@
+package An.niit.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+    @RequestMapping(value= {"/", "/home", "/index"})
+	public ModelAndView index() {
+    	
+    	ModelAndView mav =new ModelAndView("page");
+    	mav.addObject("greeting","Welcome to my Spring MVC Framework");
+    	return mav;
+		
+	}
+	
+	
+	
+	
+}
