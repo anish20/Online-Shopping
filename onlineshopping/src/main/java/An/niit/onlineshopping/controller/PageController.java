c1a8f2b4fc45fcc73bc4eca189ca11b75367f3d1
anish20 <anish15.786@gmail.com>
2017-08-30 20:10:59 +0530

    Updated Successfully again..

--- onlineshopping/src/main/java/An/niit/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/An/niit/onlineshopping/controller/PageController.java
@@ -8,14 +8,36 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-    @RequestMapping(value= {"/", "/home", "/index"})
+    
+	@RequestMapping(value= {"/", "/home", "/index"})
 	public ModelAndView index() {
     	
     	ModelAndView mav =new ModelAndView("page");
-    	mav.addObject("greeting","Welcome to my Spring MVC Framework");
+    	mav.addObject("title","Home");
+    	mav.addObject("userClickHome",true);
     	return mav;
 		
 	}
+    
+    @RequestMapping(value= "/about")
+   	public ModelAndView about() {
+       	
+       	ModelAndView mav =new ModelAndView("page");
+       	mav.addObject("title","About Us");
+       	mav.addObject("userClickAbout",true);
+       	return mav;
+   		
+   	}
+    
+    @RequestMapping(value= "/contact")
+   	public ModelAndView contact() {
+       	
+       	ModelAndView mav =new ModelAndView("page");
+       	mav.addObject("title","Contact Us");
+       	mav.addObject("userClickContact",true);
+       	return mav;
+   		
+   	}
 	
 	//This is not part of my project..its dummy..
    /* @RequestMapping(value= "/test")
---
+++ onlineshopping/src/main/webapp/WEB-INF/view/shared/navbar.jsp
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="${contextRoot}/home">Online Shopping Portal</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="nav navbar-nav ">
+            
+            <li class="nav-item">
+              <a class="nav-link" href="${contextRoot}/about">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="${contextRoot}/listProducts">View Product</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="${contextRoot}/contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
