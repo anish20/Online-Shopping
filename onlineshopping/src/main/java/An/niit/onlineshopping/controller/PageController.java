26c39bc61f5288b5409e3dcfe4a28433123af325
anish20 <anish15.786@gmail.com>
2017-08-30 17:30:03 +0530

    Successfully Updated..

--- onlineshopping/src/main/java/An/niit/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/An/niit/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package An.niit.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,7 +17,26 @@ public class PageController {
 		
 	}
 	
-	
-	
+	//This is not part of my project..its dummy..
+   /* @RequestMapping(value= "/test")
+     public ModelAndView test(@RequestParam(value="greeting",required=false )String greeting) { 
+    	 if(greeting==null) { 
+    		 greeting="Hello! there";
+    	 }
+    	ModelAndView mav =new ModelAndView("page");
+    	mav.addObject("greeting",greeting);
+    	return mav;
+		
+}*/
+  /*  @RequestMapping(value= "/test/{greeting}")
+    public ModelAndView test(@PathVariable (value="greeting",required=false )String greeting) { 
+   	 if(greeting==null) { 
+   		 greeting="Hello! there";
+   	 }
+   	ModelAndView mav =new ModelAndView("page");
+   	mav.addObject("greeting",greeting);
+   	return mav;
+		
+}*/
 	
 }
